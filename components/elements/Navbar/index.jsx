f4ee098468fc2d3ec1dca6722bbf0d59e4ea55bc
= <=>
2022-02-12 18:22:34 -0400

    navbar design completed

--- components/elements/Navbar/index.jsx
+++ components/elements/Navbar/index.jsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Nav, ItemsContainer } from "./style";
+import Image from 'next/image'
+import { Nav, LogoContainer ,ListContainer, ListElement } from "./style";
 
 const Navbar = () => {
   return (
     <Nav>
-      <ItemsContainer>
-        navbar
-      </ItemsContainer>
+      <LogoContainer>
+        <Image src="/images/disneyLogo.png" width={80} height={49} alt="disneyLogo" />
+      </LogoContainer>
+        <ListContainer>
+          <ListElement>
+            <span>
+              <Image src="/images/homeIcon.png" width={14} height={13} alt="disneyLogo" />
+            </span>
+            <a href="#">inicio</a>
+          </ListElement>
+          <ListElement>
+            <span>
+              <Image src="/images/searchIcon.png" width={13} height={13} alt="disneyLogo" />
+            </span>
+            <a href="#">búsqueda</a>
+          </ListElement>
+          <ListElement>
+            <span>
+              <Image src="/images/moviesIcon.png" width={17} height={13} alt="disneyLogo" />
+            </span>
+            <a href="#">películas</a>
+          </ListElement>
+          <ListElement>
+            <span>
+              <Image src="/images/seriesIcon.png" width={13} height={13} alt="disneyLogo" />
+            </span>
+            <a href="#">series</a>
+          </ListElement>
+        </ListContainer>
     </Nav>
   );
 };
--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -3,9 +3,16 @@ import { normalize } from 'styled-normalize'
 
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
   ${normalize}
   body {
     padding: 3rem 0 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .container-w{
+    max-width: 1000px;
+    margin: 0 auto;
   }
   `
 
